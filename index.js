319c8ab07c08c6803dbbcf78a70761cc600d07e5
Blayneraptor <cousoworks@gmail.com>
2024-03-20 13:47:30 +0100

    Añadiendo clase JavaScript

---
+++ index.js
@@ -0,0 +1,20 @@
+// Variables y tipos de datos
+
+let nombre = "Ana"; //Cadena de texto String
+let edad = 25; //Número entero Integer
+const PI = 3.14; //Constante
+
+//Operadores
+
+//let resultado = 5 / 2; //Va a salir un Double 2.5
+
+let resultado = 5 / 2; 
+let resultadoDouble = parseFloar(resultado) //Convertir a double de forma explícita
+console.log(resultadoDouble);
+
+let x = 10;
+let y = 5;
+
+let suma = x + y;
+let resta = x - y;
+let multiplicacion = x * y;
