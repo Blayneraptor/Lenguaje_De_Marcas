5f1919fd779fd994dae17f268a9689b138b49a3c
Blayneraptor <cousoworks@gmail.com>
2024-03-20 13:56:12 +0100

    Añadiendo datos JS

--- index.js
+++ index.js
@@ -18,3 +18,29 @@ let y = 5;
 let suma = x + y;
 let resta = x - y;
 let multiplicacion = x * y;
+let division = x / y;
+
+console.log(suma);
+
+//Condicionales
+
+let hora = 10;
+
+if (hora < 12) {
+    console.log("Buenos días");
+} else {
+    console.lot("Buenas tardes");
+}
+
+//Operador ternario
+
+let hora2 = 10;
+
+let mensaje = (hora < 12)? "Buenos días" : "Buenas tardes";
+console.log(mensaje);
+
+//Bucles
+
+for (let i=0; i < 5; i++){
+    console.log("Número: " + i);
+}
